ef5d7f5bccda40acdbf3ba1e3ebaeed79532ca95
Amol Pawar <amolpawar4334@gmail.com>
2025-09-06 20:50:47 +0530

    add stash

--- src/MyFirst.java
+++ src/MyFirst.java
@@ -3,6 +3,11 @@ public class MyFirst {
 
 	public static void main(String[] args) {
 		System.out.println("sd");
+		System.out.println("sd");
+		System.out.println("sd");
+		System.out.println("sd");
+		System.out.println("sd");
+		System.out.println("sd");
 
 	}
 
