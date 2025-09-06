a3fbc1c290258357b525eb01a2fd5b937c97949c
Amol Pawar <amolpawar4334@gmail.com>
2025-09-06 16:56:07 +0530

    added

--- src/MyDec.java
+++ src/MyDec.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class MyDec {
+
+public static void main(String args[]) {
+
+	//ArrayList<B> al=new ArrayList<>();
+
+	
+	System.out.println("");
+}
+
 
 }
