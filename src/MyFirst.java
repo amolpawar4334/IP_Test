52d78be4f3a599ebad8d6b78ef18136f090a8836
Amol Pawar <amolpawar4334@gmail.com>
2025-09-06 21:43:21 +0530

    Added v2

--- src/MyFirst.java
+++ src/MyFirst.java
@@ -4,6 +4,8 @@ public class MyFirst {
 	public static void main(String[] args) {
 			
 			System.out.println("Amol Pawar v1 ");
+			System.out.println("Amol Pawar v2 ");
+			System.out.println("Amol Pawar v2 ");
 
 	}
 
