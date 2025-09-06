00279c7fe122683bad2555ad047d30f12d967a31
Amol Pawar <amolpawar4334@gmail.com>
2025-09-06 22:07:43 +0530

    remove line

--- src/MyFirst.java
+++ src/MyFirst.java
@@ -4,10 +4,7 @@ public class MyFirst {
 	public static void main(String[] args) {
 			
 			System.out.println("Amol Pawar v1 ");
-			System.out.println("Amol Pawar v2 ");
-			System.out.println("Amol Pawar v2 ");
-			System.out.println("Amol Pawar v2 ");
-			System.out.println("Amol Pawar v1.1 ");
+			
 
 
 
